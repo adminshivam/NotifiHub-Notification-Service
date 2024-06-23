f2b39d55fc76032418846e7fec6e4207db075bda
adminshivam <shivam.ku.gupta@gmail.com>
2024-06-23 18:15:22 +0530

    Added redis connection and tested for same.

--- services/accumulator.service.ts
+++ services/accumulator.service.ts
@@ -1,9 +1,21 @@
-import accumulatorRepository from "../models/accumulator.model";
+import accumulatorRepository from "../repositories/accumulator.repository";
+import RedisCacheManager from "../utils/redis.util";
 
 const accumulatorService = {
   getCustomerData: async () => {
-   return await accumulatorRepository.accumulatorModel();
- }
-} 
+    await RedisCacheManager.deleteKey("abc");
+    const value = await RedisCacheManager.getKey("abc");
+    if (value) {
+      console.log(JSON.stringify(value));
+    } else {
+      const data = await accumulatorRepository.accumulatorModel();
+      console.log("Data to set in redis cache ", data);
+      await RedisCacheManager.setKey("abc", data);
+      const value = await RedisCacheManager.getKey("abc");
+      console.log(value);
+    }
+    return await accumulatorRepository.accumulatorModel();
+  },
+};
 
 export default accumulatorService;
--- utils/redis.util.ts
+++ utils/redis.util.ts
@@ -1,11 +1,50 @@
-const redis = {
-  getRedisClient: () => {
+import Redis from "ioredis";
 
-  },
+// Create a Redis client
+const redis = new Redis({
+  host: "redis-14882.c330.asia-south1-1.gce.redns.redis-cloud.com", // Redis server hostname
+  port: 14882, // Redis server port
+  username: "adminshivam", // Password (if needed)
+  password: "MKshivam@99", // Password (if needed)
+  db: 0, // Default DB number
+});
 
-  getKeyFromRedis: () => {
+redis.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+redis.on("error", (err) => {
+  console.error("Redis error: ", err);
+});
+
+const RedisCacheManager = {
+  setKey: async (key: string, value: any) => {
+    try {
+      // Set a value
+      await redis.set(key, JSON.stringify(value));
+      console.log("Key set");
+    } catch (err) {
+      console.error("Error during Redis operations:", err);
+    }
   },
-  
-  setKeyToRedis: () => {
+  getKey: async (key: string) => {
+    try {
+      // Get a value
+      const value = await redis.get(key);
+      return value ? JSON.parse(value) : null;
+    } catch (err) {
+      console.error("Error during Redis operations:", err);
+    }
   },
-}
+  deleteKey: async (key: string) => {
+    try {
+      // Delete a value
+      await redis.del(key);
+      console.log("Key deleted ", key);
+    } catch (err) {
+      console.error("Error during Redis operations:", err);
+    }
+  },
+};
+
+export default RedisCacheManager;
